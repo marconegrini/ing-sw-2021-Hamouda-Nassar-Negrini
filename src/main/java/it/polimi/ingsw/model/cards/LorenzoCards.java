c4ff9d03c60c075af705efb69944465e19854b1e
Adel <nasar.adel@gmail.com>
2021-04-10 18:47:36 +0200

    creation of Lorenzo Cards, 1st commit

---
+++ src/main/java/it/polimi/ingsw/model/cards/LorenzoCards.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw.model.cards;
+import it.polimi.ingsw.model.enumerations.LorenzoCardsType;
+
+public class LorenzoCards {
+
+    private LorenzoCardsType cardType;
+
+
+    public LorenzoCards(LorenzoCardsType cardType){
+        this.cardType=cardType;
+    }
+
+
+    public void onPickUp(){
+
+    switch (cardType){
+
+        case DISCARD2BLUEDVCARDS:
+
+            break;
+
+        case DISCARD2GREENDVCARDS:
+
+            break;
+
+        case DISCARD2VIOLETDVCARDS:
+
+            break;
+
+        case DISCARD2YELLOWDVCARDS:
+
+            break;
+
+        case TWOFAITHPOINTSCARD:
+
+            break;
+
+        case FAITHANDSHUFFLECARD:
+
+            break;
+
+    }
+
+    }
+}
