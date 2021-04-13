0e56ae010d0b998c970a107b5978776f6acb1350
Adel <nasar.adel@gmail.com>
2021-04-14 01:19:49 +0200

    Added LeaderCardFactory

---
+++ src/main/java/it/polimi/ingsw/model/leaderCardsFactory/ConcreteDiscountLeaderCardF.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.model.leaderCardsFactory;
+
+import it.polimi.ingsw.model.DataIn;
+import it.polimi.ingsw.model.cards.LeaderCard;
+import it.polimi.ingsw.model.enumerations.Resource;
+
+public class ConcreteDiscountLeaderCardF extends LeaderCardFactory{
+    private Resource discountedResourceType;
+
+    @Override
+    public LeaderCard construct(DataIn dataIn) {
+        return null;
+    }
+}
