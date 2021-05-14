e9615baaf56d28428c869cad83484c3d5d5d80e0
amir_hamouda@yahoo.com <Amir>
2021-05-14 19:37:37 +0200

    added a ping handler to keep the connection alive

--- src/main/java/it/polimi/ingsw/server/handlers/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/server/handlers/ClientHandler.java
@@ -7,7 +7,11 @@ import it.polimi.ingsw.server.TemporaryPlayer;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientHandler extends Thread{
 
@@ -27,14 +31,55 @@ public class ClientHandler extends Thread{
     public void run(){
 
         String multiplayer;
-
         String nickname;
+        //AtomicBoolean stillAlive = new AtomicBoolean(true);
 
+        Runnable runnable = () -> {
+            long oldTimeMillis = System.currentTimeMillis();
+            while (true) {
+                try {
+                    if ((System.currentTimeMillis() - oldTimeMillis) >= (2 * 1000)) {
+                        toClient.writeUTF("PING");
+                        fromClient.readUTF().toUpperCase();
+                        oldTimeMillis = System.currentTimeMillis();
+                        //throw new InterruptedIOException();
+                    }
+                } catch (SocketTimeoutException e){
+                    //e.printStackTrace();
+                    System.out.println("Inside SocketTimeoutException");
+                    if (this.temporaryPlayer != null) {
+                        Server.remove(this.temporaryPlayer);
+                        System.out.println("Removed: " + this.temporaryPlayer.getNickname());
+                    }
+                    //stillAlive.set(false);
+                    try {
+                        clientSocket.close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    break;
+                } catch (SocketException e){
+                    e.printStackTrace();
+                } catch (InterruptedIOException e){
+                    e.printStackTrace();
+                    System.out.println("Inside InterruptedException");
+                    if (this.temporaryPlayer != null) {
+                        Server.remove(this.temporaryPlayer);
+                        System.out.println("Removed: " + this.temporaryPlayer.getNickname());
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread ping = new Thread(runnable);
+        ping.start();
 
         try {
             while (true) {
 
-                while (!(fromClient.available() > 0));
+                while (!(fromClient.available() > 0)) ;
                 nickname = fromClient.readUTF();
                 if (nickname.isEmpty() || Server.nicknameAlreadyExist(nickname))
                     toClient.writeUTF("KO");
@@ -44,7 +89,7 @@ public class ClientHandler extends Thread{
                 }
             }
 
-            while (!(fromClient.available() > 0));
+            while (!(fromClient.available() > 0)) ;
             multiplayer = fromClient.readUTF();
 
             if (multiplayer.equalsIgnoreCase("YES")) {
@@ -58,7 +103,7 @@ public class ClientHandler extends Thread{
                         String command = fromClient.readUTF().toUpperCase();
 
                         if (command.equals("START") && Server.isLeader(this.temporaryPlayer)) {
-                            if(Server.size() > 1) {
+                            if (Server.size() > 1) {
                                 Server.startMultiplayerGame(Server.size());
                                 break;
                             }
@@ -76,6 +121,11 @@ public class ClientHandler extends Thread{
                 temporaryPlayer = new TemporaryPlayer(this.clientSocket, nickname, false);
                 Server.startSinglePlayergame(temporaryPlayer);
             }
+        } catch (SocketException e){
+            System.out.println("Socket closed");
+        } catch (InterruptedIOException e){
+            e.printStackTrace();
+            //Server.remove(this.temporaryPlayer);
         } catch (IOException | MaxPlayersException e) {
             e.printStackTrace();
         }
