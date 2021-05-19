a0ccf0ef49a4680ad8e64a2d993f51867e3030ca
amir_hamouda@yahoo.com <Amir>
2021-05-19 18:10:47 +0200

    This class can handle the communication with the server for the waiting room, including the ping messages

--- src/main/java/it/polimi/ingsw/client/GameConnection.java
+++ src/main/java/it/polimi/ingsw/client/GameConnection.java
@@ -5,7 +5,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -13,44 +12,147 @@ public class GameConnection {
 
     public boolean executeLobby(Socket socket, Scanner scanner, DataInputStream fromServer, DataOutputStream toServer, BufferedReader buffer) {
 
-        boolean isStarted = false;
-        try {
 
-            AtomicBoolean endThread = new AtomicBoolean(true);
-            boolean isMultiplayer;
+        AtomicBoolean isStarted = new AtomicBoolean(false);
+        AtomicBoolean endLobby = new AtomicBoolean(true);
+        AtomicBoolean endThread = new AtomicBoolean(true);
+        AtomicBoolean isMultiplayer = new AtomicBoolean();
+        AtomicBoolean threadStarted = new AtomicBoolean(false);
+        AtomicBoolean nicknameOk = new AtomicBoolean(false);
+        AtomicBoolean alreadyExecuted = new AtomicBoolean(false);
+        AtomicBoolean nicknameInserted = new AtomicBoolean(false);
 
-            System.out.println("Connecting to: " + socket);
 
-            //asking and obtaining user's nickname
-            System.out.println("Type your nickname:");
+        String message = "";
 
+        System.out.println("Connecting to:" + socket);
+
+        while (endLobby.get()) {
+
+            //waiting for a message from the server
             while (true) {
-                String nickname = scanner.nextLine();
-                toServer.writeUTF(nickname);
-                if (fromServer.readUTF().equals("OK"))
-                    break;
-                else System.out.println("Choose another nickname: ");
-            }
-
-            System.out.println("Start a multiplayer game? [yes/no]");
-
-            while (true) {
-                String multiplayer = scanner.nextLine();
-
-                if ((multiplayer.isEmpty() || !multiplayer.equalsIgnoreCase("YES"))
-                        && (multiplayer.isEmpty() || !multiplayer.equalsIgnoreCase("NO"))) {
-                    System.out.println("Type again your choice: ");
-                } else {
-                    isMultiplayer = multiplayer.equalsIgnoreCase("YES");
-                    toServer.writeUTF(multiplayer);
-                    break;
+                try {
+                    if (fromServer.available() > 0) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
 
-            if (isMultiplayer) {
+            //obtaining the message from the server
+            try {
+                message = fromServer.readUTF();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-                System.out.println("Added to players list.\nType 'EXIT' to leave the game");
+            if (message.toUpperCase().contains("PING")) {
+                Runnable runnable = () -> {
+                    try {
+                        toServer.writeUTF("PING");
+                        System.out.println("Ricevuto e ripingato");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                };
 
+                Thread startGame = new Thread(runnable);
+                startGame.start();
+            }
+
+            //starting to analyze the message
+            if (message.toUpperCase().contains("NICKNAME")) {
+
+                if (message.toUpperCase().contains("OK")) {
+                    nicknameOk.set(true);
+                }
+
+                if (message.toUpperCase().contains("KO")) {
+                    System.out.println("Choose another nickname: ");
+                    nicknameOk.set(false);
+                }
+
+                if (!nicknameInserted.get()) {
+                    System.out.println("Type your nickname:");
+                }
+
+                Runnable runnable = () -> {
+
+                    try {
+                        while (!nicknameOk.get()) {
+                            String nickname = scanner.nextLine();
+                            toServer.writeUTF("NICKNAME" + nickname);
+                            nicknameOk.set(true);
+                            nicknameInserted.set(true);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                };
+
+                Thread nickname = new Thread(runnable);
+                nickname.start();
+                alreadyExecuted.set(true);
+            }
+
+            if (message.toUpperCase().contains("MULTIPLAYER")) {
+
+                if (message.toUpperCase().contains("OK")) {
+                    if (isMultiplayer.get())
+                        System.out.println("Added to players list.\nType 'EXIT' to leave the game");
+                    else endLobby.set(false);
+                } else {
+                    Runnable runnable = () -> {
+                        System.out.println("Start a multiplayer game? [yes/no]");
+
+                        while (true) {
+                            try {
+                                String multiplayer = scanner.nextLine();
+                                if ((multiplayer.isEmpty() || !multiplayer.equalsIgnoreCase("YES"))
+                                        && (multiplayer.isEmpty() || !multiplayer.equalsIgnoreCase("NO"))) {
+                                    System.out.println("Type again your choice: ");
+                                } else {
+                                    isMultiplayer.set(multiplayer.equalsIgnoreCase("YES"));
+                                    try {
+                                        toServer.writeUTF("MULTIPLAYER" + multiplayer);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                }
+                            } catch (IndexOutOfBoundsException e) {
+                                System.out.println("Catchata: " + e.getMessage());
+                                break;
+                            }
+                        }
+                    };
+                    Thread multiplayer = new Thread(runnable);
+                    multiplayer.start();
+                }
+            }
+
+            if (message.toUpperCase().contains("LEADER")) {
+
+                Runnable runnable = () -> {
+                    System.out.println("You are the leader: Type START to start the game");
+                };
+
+                Thread leader = new Thread(runnable);
+                leader.start();
+            }
+
+            if (message.toUpperCase().contains("WAITING")) {
+                String data = message;
+                Runnable runnable = () -> {
+                    System.out.println("Waiting with other: " + data.charAt(0) + " players");
+                };
+
+                Thread leader = new Thread(runnable);
+                leader.start();
+            }
+
+
+            if (!threadStarted.get() && isMultiplayer.get()) {
+                threadStarted.set(true);
                 Runnable runnable1 = () -> {
                     try {
                         String toExit;
@@ -58,105 +160,57 @@ public class GameConnection {
 
                             if (buffer.ready()) {
                                 toExit = buffer.readLine();
-                                if (toExit.equalsIgnoreCase("EXIT") || toExit.equalsIgnoreCase("START")) {
+                                if (toExit.equalsIgnoreCase("EXIT")) {
                                     toServer.writeUTF(toExit);
                                     buffer.close();
                                     endThread.set(false);
-                                }
+                                } else if (toExit.equalsIgnoreCase("START")) toServer.writeUTF(toExit);
+                                else System.out.println("Bad command. Type another command");
                             }
                         }
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        endThread.set(false);
                     }
                 };
-
                 Thread exit = new Thread(runnable1);
                 exit.start();
-
-                String notification = "";
-
-                while (true) {
-                    try {
-                        while (!(fromServer.available() > 0)) ;
-                        notification = fromServer.readUTF();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (notification.equalsIgnoreCase("LEADER"))
-                        System.out.println("You are the game leader: type 'START' to start the game.");
-
-                    if (notification.toUpperCase().contains("WAITING"))
-                        System.out.println("Waiting with other: " + notification.charAt(0) + " players");
-
-
-                    if (notification.equalsIgnoreCase("EXIT")) {
-                        System.out.println("Exiting the game...");
-                        buffer.close();
-                        isStarted = false;
-                        break;
-                    }
-
-                    if (notification.equalsIgnoreCase("GAME STARTED")) {
-                        endThread.set(false);
-                        isStarted = true;
-                        break;
-                    }
-                }
-
-                if (isStarted) {
-
-                    while (true) {
-                        while (!(fromServer.available() > 0)) ;
-                        try {
-                            notification = fromServer.readUTF();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        if (notification.equalsIgnoreCase("OK IN GAME")) {
-                            System.out.println("You are inside the game");
-                            break;
-                        }
-                    }
-                }
-
-            } else {
-
-                String notification = "";
-
-                while (true) {
-                    try {
-                        notification = fromServer.readUTF();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (notification.equalsIgnoreCase("GAME STARTED")) {
-                        System.out.println("The game is starting...");
-                        break;
-                    }
-                }
-                if (isStarted) {
-
-                    while (true) {
-                        try {
-                            notification = fromServer.readUTF();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        if (notification.equalsIgnoreCase("OK IN GAME")) {
-                            System.out.println("You are inside the game with Lorenzo");
-                            break;
-                        }
-                    }
-                }
             }
 
+            if (message.toUpperCase().contains("EXIT")) {
+
+                System.out.println("Exiting the game...");
+                endLobby.set(false);
+            }
+
+            if (message.toUpperCase().contains("GAME STARTED")) {
+
+                try {
+                    toServer.writeUTF("PINGKO");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Runnable runnable = () -> {
+                    endThread.set(false);
+                    isStarted.set(true);
+                };
+
+                Thread startGame = new Thread(runnable);
+                startGame.start();
+
+                if (isMultiplayer.get()) {
+                    System.out.println("You are inside the game");
+                } else {
+                    System.out.println("You are inside the game with Lorenzo");
+                }
+                endLobby.set(false);
+            }
+        }
+        try {
             buffer.close();
             scanner.close();
-        } catch (SocketException e) {
-            System.out.println("Connection lost");
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return isStarted;
+        return isStarted.get();
     }
 }
