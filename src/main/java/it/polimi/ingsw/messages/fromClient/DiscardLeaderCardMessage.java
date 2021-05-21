4f3cc147ade5008e9c33345eddaacf7c17040860
amir_hamouda@yahoo.com <Amir>
2021-05-21 20:01:01 +0200

    Message class that handle the discard leader card action

---
+++ src/main/java/it/polimi/ingsw/messages/fromClient/DiscardLeaderCardMessage.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.messages.fromClient;
+
+import it.polimi.ingsw.server.handlers.ClientHandler;
+
+public class DiscardLeaderCardMessage extends ClientMessage{
+
+    Integer slot;
+
+    /**
+     *
+     * @param slot  slot is the position of the leader card that the player wants to discard
+     */
+    public DiscardLeaderCardMessage(Integer slot) {
+        super(ClientMessageType.DISCARD_LEADERCARD);
+        this.slot = slot;
+    }
+
+    @Override
+    public void serverProcess(ClientHandler clientHandler) {
+
+    }
+}
