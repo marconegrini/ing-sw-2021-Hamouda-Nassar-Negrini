319793d3ef10b704dc0772312bfaae916f30e0bf
amir_hamouda@yahoo.com <Amir>
2021-06-02 20:09:34 +0200

    Added new methods

--- src/main/java/it/polimi/ingsw/client/gui/SceneManager.java
+++ src/main/java/it/polimi/ingsw/client/gui/SceneManager.java
@@ -1,23 +1,40 @@
 package it.polimi.ingsw.client.gui;
 
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class SceneManager {
 
     private static Stage primaryStage;
+    private static Scene scene;
 
-    public SceneManager(Stage primaryStage){
-        this.primaryStage = primaryStage;
-    }
 
-    public static void setScene(Scene scene){
-        primaryStage.setScene(scene);
+    public static void setScene(Scene newScene){
+        scene = newScene;
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                primaryStage.setScene(newScene);
+            }
+        });
     }
 
     public static void setPrimaryStage(Stage stage){
         primaryStage = stage;
     }
 
+    public static Stage getPrimaryStage(){
+        return primaryStage;
+    }
+
+    public static void setLabelText(String s){
+        Label participantsNumber = (Label) primaryStage.getScene().lookup(s);
+    }
+
+    public static Scene getScene(){
+        return scene;
+    }
 
 }
