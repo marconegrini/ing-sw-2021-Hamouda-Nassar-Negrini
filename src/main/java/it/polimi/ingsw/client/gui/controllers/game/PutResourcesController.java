020bb1ba0ac6bbe94881b53117b8a2c40420e282
amir_hamouda@yahoo.com <Amir>
2021-06-19 23:51:24 +0200

    Classes that allow the user to pick resources

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/game/PutResourcesController.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.client.gui.controllers.game;
+
+import javafx.scene.input.MouseEvent;
+
+public class PutResourcesController {
+
+    public void selectedFirstShelf(MouseEvent mouseEvent) {
+    }
+
+    public void selectedSecondShelf(MouseEvent mouseEvent) {
+    }
+
+    public void selectedThirdShelf(MouseEvent mouseEvent) {
+    }
+}
