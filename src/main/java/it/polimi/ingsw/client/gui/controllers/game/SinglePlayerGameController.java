10da78d4abe19661b1281310a5d7fadfc40f8193
amir_hamouda@yahoo.com <Amir>
2021-06-30 22:46:27 +0200

    single player gui controller

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/game/SinglePlayerGameController.java
@@ -0,0 +1,112 @@
+package it.polimi.ingsw.client.gui.controllers.game;
+
+import it.polimi.ingsw.client.gui.SceneManager;
+import it.polimi.ingsw.client.gui.UpdateObjects;
+import it.polimi.ingsw.client.gui.controllers.ControllerGUI;
+import it.polimi.ingsw.model.MarketBoard;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SinglePlayerGameController {
+
+
+    public void showMarketBoard(ActionEvent actionEvent) {
+        Platform.runLater(() ->{
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/game/marketboard.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            newStage.setTitle("Market board");
+            Scene scene = new Scene(root, 600,600);
+            SceneManager.setPopUpScene(scene);
+            newStage.setScene(scene);
+            newStage.resizableProperty().setValue(Boolean.FALSE);
+            newStage.setOnCloseRequest( event ->{ event.consume();});
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            MarketBoard marketBoard = ControllerGUI.getServerHandler().getLightModel().getMarketBoard();
+            newStage.show();
+            UpdateObjects.updateLeaderCards(ControllerGUI.getServerHandler().getLightModel().getLeaderCards(), scene);
+            UpdateObjects.updateMarketBoard(marketBoard, scene);
+        });
+    }
+
+    public void activateProduction(ActionEvent actionEvent) {
+        Platform.runLater(() ->{
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/game/activateProduction/activateProduction.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            newStage.setTitle("Activate production");
+            Scene scene = new Scene(root, 1300,670);
+            SceneManager.setPopUpScene(scene);
+            newStage.setScene(scene);
+            newStage.resizableProperty().setValue(Boolean.FALSE);
+            newStage.setOnCloseRequest( event ->{ event.consume();});
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.show();
+            UpdateObjects.updateLeaderCards(ControllerGUI.getServerHandler().getLightModel().getLeaderCards(), scene);
+            UpdateObjects.updateDevCardsSlot(ControllerGUI.getServerHandler().getLightModel().getPeekDevCardsInSlot(), scene, false);
+        });
+    }
+
+    public void ActivateLeaderCard(ActionEvent actionEvent) {
+        Platform.runLater(() ->{
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/game/activateLeaderCard.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            newStage.setTitle("Activate Leader Card");
+            Scene scene = new Scene(root, 1080,670);
+            SceneManager.setPopUpScene(scene);
+            newStage.setScene(scene);
+            newStage.resizableProperty().setValue(Boolean.FALSE);
+            newStage.setOnCloseRequest( event ->{ event.consume();});
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.show();
+            UpdateObjects.updateLeaderCards(ControllerGUI.getServerHandler().getLightModel().getLeaderCards(), scene);
+        });
+    }
+
+    public void BuyDevelopmentCard(ActionEvent actionEvent) {
+        Platform.runLater(() ->{
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/game/buyDevCard.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            newStage.setTitle("Buy Development Card");
+            Scene scene = new Scene(root, 1080,670);
+            SceneManager.setPopUpScene(scene);
+            newStage.setScene(scene);
+            newStage.resizableProperty().setValue(Boolean.FALSE);
+            newStage.setOnCloseRequest( event ->{ event.consume();});
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.show();
+            UpdateObjects.updateDevCardsDeck(ControllerGUI.getServerHandler().getLightModel().getDevelopmentCardsDeck(), scene);
+            UpdateObjects.updateResources(ControllerGUI.getServerHandler().getLightModel().getCoffer(), ControllerGUI.getServerHandler().getLightModel().getWarehouse(), scene);
+        });
+    }
+}
