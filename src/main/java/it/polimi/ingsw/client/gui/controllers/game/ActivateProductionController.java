997547c7e486949898c1b3c94b3f43c3026cc4a5
amir_hamouda@yahoo.com <Amir>
2021-07-01 13:12:32 +0200

    implemented personal production functionality

--- src/main/java/it/polimi/ingsw/client/gui/controllers/game/ActivateProductionController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/game/ActivateProductionController.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.client.gui.controllers.game;
 
 import it.polimi.ingsw.client.gui.SceneManager;
+import it.polimi.ingsw.client.gui.UpdateObjects;
 import it.polimi.ingsw.client.gui.controllers.ControllerGUI;
+import it.polimi.ingsw.enumerations.CardType;
+import it.polimi.ingsw.enumerations.Resource;
+import it.polimi.ingsw.messages.fromClient.ActivateProductionMessage;
+import it.polimi.ingsw.model.cards.LeaderCard;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -25,10 +31,19 @@ import java.util.List;
  */
 public class ActivateProductionController {
 
+    public Label label1;
+    public Label servant1;
+    public Label coin1;
+    public Label shield1;
+    @FXML
+    private Label leaderCard2;
+    @FXML
+    private Label stone2, shield2, coin2, servant2, label2;
     @FXML
     private Label personalProduction;
     private boolean selectedPersonalProduction = false;
-    private List<Integer> selectedSlots = new ArrayList<>();
+    private static List<Integer> selectedSlots = new ArrayList<>();
+    private Label selectedLabel1, selectedLabel2;
 
     public void selectedDevCardSlot(MouseEvent mouseEvent) {
         Label label;
@@ -56,6 +71,8 @@ public class ActivateProductionController {
         Node source = (Node) actionEvent.getSource();
         Window theStage = source.getScene().getWindow();
         theStage.hide();
+
+        selectedSlots.clear();
 
         if (!ControllerGUI.getServerHandler().getIsMultiplayer())   return;
 
@@ -93,5 +110,199 @@ public class ActivateProductionController {
 
     public void activate(ActionEvent actionEvent) {
         System.out.println(selectedSlots);
+
+        if (selectedSlots.isEmpty())    return;
+
+        int activatedLeaderCards = 0;
+
+        for (LeaderCard lc : ControllerGUI.getServerHandler().getLightModel().getLeaderCards())
+            if (lc.getCardType().equals(CardType.PRODUCTION) && lc.isActivated()) activatedLeaderCards++;
+
+        if (activatedLeaderCards > 0) {
+            Platform.runLater(() -> {
+                Stage newStage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/game/useProductionCards.fxml"));
+                Parent root = null;
+                try {
+                    root = loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                newStage.setTitle("Production cards");
+                Scene scene = new Scene(root, 450, 250);
+                SceneManager.setPopUpScene(scene);
+                newStage.setScene(scene);
+                newStage.resizableProperty().setValue(Boolean.FALSE);
+                newStage.setOnCloseRequest(event -> {
+                    event.consume();
+                });
+                newStage.initModality(Modality.APPLICATION_MODAL);
+                newStage.show();
+            });
+        } else {
+            ControllerGUI.getServerHandler().sendJson(new ActivateProductionMessage(selectedSlots, null));
+        }
+
+        selectedSlots.clear();
+
+        Node source = (Node) actionEvent.getSource();
+        Window theStage = source.getScene().getWindow();
+        theStage.hide();
+
+    }
+
+    public void useProductionCards(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
+            Stage newStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/game/selectLeaderResources.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            newStage.setTitle("Leader Resources");
+            Scene scene = new Scene(root, 1080, 670);
+            SceneManager.setPopUpScene(scene);
+            newStage.setScene(scene);
+            newStage.resizableProperty().setValue(Boolean.FALSE);
+            newStage.setOnCloseRequest(event -> {
+                event.consume();
+            });
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.show();
+
+            int activatedLeaderCards = 1;
+            for (LeaderCard lc : ControllerGUI.getServerHandler().getLightModel().getLeaderCards())
+                if (lc.getCardType().equals(CardType.PRODUCTION) && lc.isActivated()){
+                    Node source = (Node) actionEvent.getSource();
+                    Scene theScene = SceneManager.getPopUpScene();
+                    Label card = (Label) scene.lookup("#leaderCard"+activatedLeaderCards);
+                    card.setStyle("-fx-background-image: url(\"images/leadercards/" +
+                            lc.toPath() + ".png\");" +
+                            " -fx-background-size: 100% 100%;" +
+                            "-fx-border-width: 5");
+
+                    Label label = (Label) scene.lookup("#servant"+activatedLeaderCards);
+                    label.setOpacity(1);
+                    label = (Label) scene.lookup("#coin"+activatedLeaderCards);
+                    label.setOpacity(1);
+                    label = (Label) scene.lookup("#shield"+activatedLeaderCards);
+                    label.setOpacity(1);
+                    label = (Label) scene.lookup("#stone"+activatedLeaderCards);
+                    label.setOpacity(1);
+                    label = (Label) scene.lookup("#label"+activatedLeaderCards);
+                    label.setOpacity(1);
+
+                    activatedLeaderCards++;
+                }
+
+            UpdateObjects.updateLeaderCards(ControllerGUI.getServerHandler().getLightModel().getLeaderCards(), scene);
+            /*if (activatedLeaderCards == 1){
+                stone2.setOpacity(0);
+                shield2.setOpacity(0);
+                coin2.setOpacity(0);
+                servant2.setOpacity(0);
+                label2.setOpacity(0);
+                leaderCard2.setOpacity(0);
+            }
+
+             */
+        });
+        Node source = (Node) actionEvent.getSource();
+        Window theStage = source.getScene().getWindow();
+        theStage.hide();
+
+    }
+
+    public void dontUseProductionCards(ActionEvent actionEvent) {
+        System.out.println("dont use: " + selectedSlots);
+        ControllerGUI.getServerHandler().sendJson(new ActivateProductionMessage(selectedSlots, null));
+        selectedSlots.clear();
+        Node source = (Node) actionEvent.getSource();
+        Window theStage = source.getScene().getWindow();
+        theStage.hide();
+    }
+
+    public void select(ActionEvent actionEvent) {
+        System.out.println("select: " + selectedSlots);
+        List<Resource> resources = new ArrayList<>();
+
+        if (selectedLabel1 == null){
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("ERROR");
+                alert.setHeaderText("Error: Resource");
+                alert.setContentText("You have to select an output resource from the first column.");
+                alert.showAndWait();
+            });
+            return;
+        }
+
+        int activatedLeaderCards = 0;
+        for (LeaderCard lc : ControllerGUI.getServerHandler().getLightModel().getLeaderCards())
+            if (lc.getCardType().equals(CardType.PRODUCTION) && lc.isActivated()) activatedLeaderCards++;
+
+        if (activatedLeaderCards > 1){
+            if (selectedLabel2 == null){
+                Platform.runLater(() -> {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("ERROR");
+                    alert.setHeaderText("Error: Resource");
+                    alert.setContentText("You have to select an output resource from the second column.");
+                    alert.showAndWait();
+                });
+                return;
+            }
+        }
+        Node source = (Node) actionEvent.getSource();
+        Window theStage = source.getScene().getWindow();
+        theStage.hide();
+
+        resources.add(Resource.getEnum(getResource(selectedLabel1)));
+        resources.add(Resource.getEnum(getResource(selectedLabel2)));
+        //System.out.println(resources);
+        ControllerGUI.getServerHandler().sendJson(new ActivateProductionMessage(selectedSlots, resources));
+        selectedSlots.clear();
+    }
+
+    public void selectLabel1(MouseEvent mouseEvent) {
+
+        Label label = (Label) mouseEvent.getPickResult().getIntersectedNode();
+
+        if (selectedLabel1 == null){
+            selectedLabel1 = label;
+            selectedLabel1.getStyleClass().add("selectedCard");
+        } else{
+            selectedLabel1.getStyleClass().remove("selectedCard");
+            selectedLabel1 = label;
+            selectedLabel1.getStyleClass().add("selectedCard");
+        }
+    }
+
+    public void selectLabel2(MouseEvent mouseEvent) {
+        Label label = (Label) mouseEvent.getPickResult().getIntersectedNode();
+
+        if (selectedLabel2 == null){
+            selectedLabel2 = label;
+            selectedLabel2.getStyleClass().add("selectedCard");
+        } else{
+            selectedLabel2.getStyleClass().remove("selectedCard");
+            selectedLabel2 = label;
+            selectedLabel2.getStyleClass().add("selectedCard");
+        }
+    }
+
+    /**
+     * This method is used to get a string from the styleSheet of a label
+     * @param label  is the label from which you want to retrieve the string
+     * @return  the string retrieved from the label
+     */
+    private String getResource(Label label) {
+        if (label.getStyleClass().contains("coin")) return "coin";
+        if (label.getStyleClass().contains("shield")) return "shield";
+        if (label.getStyleClass().contains("stone")) return "stone";
+        if (label.getStyleClass().contains("servant")) return "servant";
+        return "";
     }
 }
